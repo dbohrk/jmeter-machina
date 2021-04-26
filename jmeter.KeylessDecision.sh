140730aa231215f635478d62de1dfa3b32b49068
Dave Bohrk <dave@ionicsecurity.com>
2021-04-26 18:04:23 -0400

    Modified for headless operation

--- jmeter.KeylessDecision.sh
+++ jmeter.KeylessDecision.sh
@@ -1,8 +1,18 @@
 # Keyless Decision
-rm KeylessDecision.csv
-rm -rf Report-KeylessDecision/*
-jmeter -n -t KeylessDecision/KeylessDecision.jmx -l KeylessDecision.csv -JkeylessDecisionUsers=20 -Jrampup=5 -Jseconds=90
-jmeter -g KeylessDecision.csv -o Report-KeylessDecision
+rm Data/KeylessDecision-without.csv
+rm -rf Reports/KeylessDecision-without/*
+jmeter -n -t KeylessDecision/KeylessDecision.jmx -l Data/KeylessDecision-without.csv -JkeylessDecisionUsers=20 -Jrampup=5 -Jseconds=1800
+jmeter -g Data/KeylessDecision-without.csv -o Reports/KeylessDecision-without
+# 
+# Keyless Decision with Keyless Policy Decision
+# rm Data/KeylessDecision.csv
+# rm -rf Reports/KeylessDecision/*
+# rm Data/KeylessDecision-with.csv
+# rm -rf Reports/KeylessDecision-with/*
+# jmeter -n -t KeylessDecision/KeylessDecision.jmx -l Data/KeylessDecision.csv -JkeylessDecisionUsers=20 -Jrampup=5 -Jseconds=1800&
+# jmeter -n -t KeylessDecision/KeylessDecision.jmx -l Data/KeylessDecision-with.csv -JkeylessDecisionUsers=20 -Jrampup=5 -Jseconds=1800
+# jmeter -g Data/KeylessDecision.csv -o Reports/KeylessDecision&
+# jmeter -g Data/KeylessDecision-with.csv -o Reports/KeylessDecision-with&
 #
 # Add Users
 # jmeter -n -t KeylessDecision/KeylessDecision.jmx -l headless.csv -JaddUsersUsers=20 -Jrampup=5 -Jseconds=90
