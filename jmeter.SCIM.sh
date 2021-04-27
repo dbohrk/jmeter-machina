e793c2a7ae99ffffbf1930b8ee895e7f1eb51b44
Dave Bohrk <dave@ionicsecurity.com>
2021-04-27 15:27:39 -0400

    Fixed script errors with threadGroup variable

--- jmeter.SCIM.sh
+++ jmeter.SCIM.sh
@@ -2,17 +2,22 @@ baseDirectory="./"
 reportsDirectory="reports"
 dataDirectory="data"
 testPlanDirectory="jmx"
-testPlan=KeylessDecision
+testPlan="SCIM"
 
 jmeterUsers=20
 jmaterRampup=5
 jmeterSeconds=90
 
-threadGroups=( "keylessDecision" "addUsers" "addPolicies" "deletePolicies" )
+# threadGroups=( "listScopes" "listDevices" "fetchDeviceDynamic" "fetchDeviceStatic" "addUsersInGroup" "addUsers" "listUsers" "deleteUsers" "fetchUserDynamic" "fetchUserStatic" \
+#"listRoles" "fetchRoleDynamic" "fetchRoleStatic" "createGroups" "listGroups" "deleteGroups" "fetchGroupDynamic" "fetchGroupStatic" "updateGroupPutDynamic" "updateGroupPutStatic" \
+#"updateGroupPatchDynamic" "updateGroupPatchStatic" )
+
+threadGroups=( "listScopes" "listDevices" )
 
 for threadGroup in "${threadGroups[@]}"
 do
-	echo "***** Thread Group: "$threadGroup" *****"
+	echo $threadGroup"Users"
+
 	rm $dataDirectory/$threadGroup.csv
 	rm -rf $reportsDirectory/$threadGroup/*
 	jmeter -n -t $testPlanDirectory/$testPlan.jmx -l $dataDirectory/$threadGroup.csv -J$threadGroup"Users"=$jmeterUsers -Jrampup=$jmeterRampup -Jseconds=$jmeterSeconds
