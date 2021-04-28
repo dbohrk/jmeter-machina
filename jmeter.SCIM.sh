9d5935d2115e9ff4c4d1be43267873c7f9239940
Dave Bohrk <dave@ionicsecurity.com>
2021-04-27 20:23:20 -0400

    Fixed issue with parallel JMeter processes not both going to background.

--- jmeter.SCIM.sh
+++ jmeter.SCIM.sh
@@ -3,24 +3,48 @@ reportsDirectory="reports"
 dataDirectory="data"
 testPlanDirectory="jmx"
 testPlan="SCIM"
+parallelTestPlan="KeylessDecision"
+parallelThreadGroup="keylessDecision"
 
 jmeterUsers=20
 jmaterRampup=5
-jmeterSeconds=90
+jmeterSeconds=1800
 
-threadGroups=( "listScopes" "listDevices" "fetchDeviceDynamic" "fetchDeviceStatic" "addUsersInGroup" "addUsers" "listUsers" "deleteUsers" "fetchUserDynamic" "fetchUserStatic" \
-	"listRoles" "fetchRoleDynamic" "fetchRoleStatic" "createGroups" "listGroups" "deleteGroups" "fetchGroupDynamic" "fetchGroupStatic" "updateGroupPutDynamic" "updateGroupPutStatic" \
-	"updateGroupPatchDynamic" "updateGroupPatchStatic" )
+#threadGroups=( "listScopes" "listDevices" "fetchDeviceDynamic" "fetchDeviceStatic" "addUsersInGroup" "addUsers" "listUsers" "deleteUsers" "fetchUserDynamic" "fetchUserStatic" \
+#	"listRoles" "fetchRoleDynamic" "fetchRoleStatic" "createGroups" "listGroups" "deleteGroups" "fetchGroupDynamic" "fetchGroupStatic" "updateGroupPutDynamic" "updateGroupPutStatic" \
+#	"updateGroupPatchDynamic" "updateGroupPatchStatic" )
 
-# threadGroups=( "listScopes" "listDevices" )
+threadGroups=( "listScopes" "listDevices" )
 
 for threadGroup in "${threadGroups[@]}"
 do
-	echo "***** Thread Group: "$threadGroup" *****"
+	echo '***** Thread Group: '$threadGroup' without parallel load*****'
 
 	rm $dataDirectory/$threadGroup.csv
 	rm -rf $reportsDirectory/$threadGroup/*
 	jmeter -n -t $testPlanDirectory/$testPlan.jmx -l $dataDirectory/$threadGroup.csv -J$threadGroup"Users"=$jmeterUsers -Jrampup=$jmeterRampup -Jseconds=$jmeterSeconds
-	jmeter -g $dataDirectory/$threadGroup.csv -o $reportsDirectory/$threadGroup
+	jmeter -g $dataDirectory/$threadGroup.csv -o $reportsDirectory/$threadGroupi
+
+
+        echo '***** Thread Group: '$threadGroup' with '$parallelThreadGroup' load *****'
+
+        rm $dataDirectory/$threadGroup-with-$parallelThreadGroup.csv
+        rm -rf $reportsDirectory/$threadGroup-with-$parallelThreadGroup/*
+
+        rm $dataDirectory/$parallelThreadGroup-with-$threadGroup.csv
+        rm -rf $reportsDirectory/$parallelThreadGroup-with-$threadGroup/*
+
+        jmeter -n -t $testPlanDirectory/$testPlan.jmx -l $dataDirectory/$threadGroup-with-$parallelThreadGroup.csv -J$threadGroup"Users"=$jmeterUsers -Jrampup=$jmeterRampup -Jseconds=$jmeterSeconds&
+
+#	Start Parallel Test Plan and Thread Group
+
+        jmeter -n -t $testPlanDirectory/$parallelTestPlan.jmx -l $dataDirectory/$parallelThreadGroup-with-$threadGroup.csv -J$parallelThreadGroup"Users"=$jmeterUsers -Jrampup=$jmeterRampup -Jseconds=$jmeterSeconds&
+
+#	Wait for first JMeter run to finish
+	wait
+#	Produce JMeter reports
+
+        jmeter -g $dataDirectory/$threadGroup-with-$parallelThreadGroup.csv -o $reportsDirectory/$threadGroup-with-$parallelThreadGroup
+        jmeter -g $dataDirectory/$parallelThreadGroup-with-$threadGroup.csv -o $reportsDirectory/$parallelThreadGroup-with-$threadGroup
 done
 
